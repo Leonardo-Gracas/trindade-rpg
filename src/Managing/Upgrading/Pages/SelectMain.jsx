534994e9e9e14b63b4a2c30dd9cf7b1233b06150
Leonardo Graças <leonardo.gracas@AES.LOCAL>
2024-09-12 17:56:35 -0300

    Correção das cores dos atributos principais em evolução

--- src/Managing/Upgrading/Pages/SelectMain.jsx
+++ src/Managing/Upgrading/Pages/SelectMain.jsx
@@ -39,26 +39,20 @@ const SelectMain = ({ setData, handleClose }) => {
             <ModalBody>
                 <h4 className='text-center mb-4'>O que mudou em seu ser?</h4>
 
-                <Nav className='d-flex flex-column' style={{ color: "black" }}>
-                    <Nav.Item>
-                        <Nav.Link eventKey={0} id='atletismo' className='btn btn-outline-dark mb-3 border-1' onClick={() => handleChoice(0)}>
-                            <i className='bi fs-3 bi-circle'></i>
-                            <h5>Atletismo</h5>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey={1} id='presença' className='btn btn-outline-dark mb-3' onClick={() => handleChoice(1)}>
-                            <i className='bi fs-3 bi-triangle'></i>
-                            <h5>Presença</h5>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <NavLink eventKey={2} id='Estudo' className='btn btn-outline-dark' onClick={() => handleChoice(2)}>
-                            <i className='bi fs-3 bi-square'></i>
-                            <h5>Estudo</h5>
-                        </NavLink>
-                    </Nav.Item>
-                </Nav>
+                <div className='d-flex flex-column' style={{ color: "black" }}>
+                    <div eventKey={0} id='atletismo' className={`btn btn-outline-dark mb-3 ${choice == 0 ? "active" : ""}`} onClick={() => handleChoice(0)}>
+                        <i className='bi fs-3 bi-circle'></i>
+                        <h5>Atletismo</h5>
+                    </div>
+                    <div eventKey={1} id='presença' className={`btn btn-outline-dark mb-3 ${choice == 1 ? "active" : ""}`} onClick={() => handleChoice(1)}>
+                        <i className='bi fs-3 bi-triangle'></i>
+                        <h5>Presença</h5>
+                    </div>
+                    <div eventKey={2} id='Estudo' className={`btn btn-outline-dark ${choice == 2 ? "active" : ""}`} onClick={() => handleChoice(2)}>
+                        <i className='bi fs-3 bi-square'></i>
+                        <h5>Estudo</h5>
+                    </div>
+                </div>
             </ModalBody>
             <ModalFooter>
                 <button className='btn btn-danger' style={{ width: '150px' }} onClick={handleClose}>Cancelar</button>
